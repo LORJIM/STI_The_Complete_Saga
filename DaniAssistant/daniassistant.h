6b864c2734c623a53f984d1acabbf2a494a94fbe
LORJIM <55024467+LORJIM@users.noreply.github.com>
2021-09-10 17:11:24 +0200

    DLL y Juegos Impetertinentes Dani Assistant

---
+++ DaniAssistant/daniassistant.h
@@ -0,0 +1,28 @@
+#ifndef DANIASSISTANT_H
+#define DANIASSISTANT_H
+
+#include "DaniAssistant_global.h"
+#include <QSystemTrayIcon>
+
+class DANIASSISTANT_EXPORT DaniAssistant : public QObject
+{
+    Q_OBJECT
+public:
+    DaniAssistant(QWidget *parent,QSystemTrayIcon *trayIcon);
+    DaniAssistant(QWidget *parent=nullptr);
+    ~DaniAssistant(void){};
+signals:
+    void mouseMovedSignal();
+private:
+    QSystemTrayIcon *sistemaNotis;
+    QWidget *appPrincipal;
+private slots:
+    void notiBienvenida();
+    void insist();
+    void advertencia();
+    void mouseMoved();
+    void juego();
+    void pintar();
+};
+
+#endif // DANIASSISTANT_H
